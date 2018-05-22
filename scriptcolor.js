0aba524514d9fb73b4c00cbd557514551b17ab85
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-05-21 21:35:16 -0400

    Update scriptcolor.js

--- scriptcolor.js
+++ scriptcolor.js
@@ -15,7 +15,7 @@ myapp.name=document.getElementsByName("name1")[0].value;
 };
 window.addEventListener("load", handler);
 function handler() {
-const w = (screen.width)/2;
+const w = (screen.width);
 const h = (screen.width)/2;
 const svg = d3.select("#ten11")
             .append('svg')
