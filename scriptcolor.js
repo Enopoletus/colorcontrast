54837f8fe9f07a2975cc52061510d09399ea6a56
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-05-24 00:00:53 -0400

    Update scriptcolor.js

--- scriptcolor.js
+++ scriptcolor.js
@@ -45,8 +45,8 @@ document.getElementById("smean1").value=Number((Math.sqrt(722*((myapp.score3/255
 const un=document.getElementById("ucount3").value=Number((Math.sqrt((myapp.pscore3*100)*((myapp.score9/255)**2)+(myapp.pscore2*100)*((myapp.score8/255)**2)+(myapp.pscore1*100)*((myapp.score7/255)**2))).toFixed(1));
 document.getElementById("ucount2").value=Number((Math.sqrt((myapp.pscore3*100)*((myapp.score6/255)**2)+(myapp.pscore2*100)*((myapp.score5/255)**2)+(myapp.pscore1*100)*((myapp.score4/255)**2))).toFixed(1));
 document.getElementById("ucount1").value=Number((Math.sqrt((myapp.pscore3*100)*((myapp.score3/255)**2)+(myapp.pscore2*100)*((myapp.score2/255)**2)+(myapp.pscore1*100)*((myapp.score1/255)**2))).toFixed(1));
-        const az=[(myapp.score1)**2-(myapp.score3)**2, (myapp.score2)**2-(myapp.score3)**2, -((255/10)**2)]
-                 [(myapp.score4)**2-(myapp.score6)**2, (myapp.score5)**2-(myapp.score6)**2, -((255/10)**2)]
+        const az=[[(myapp.score1)**2-(myapp.score3)**2, (myapp.score2)**2-(myapp.score3)**2, -((255/10)**2)],
+                 [(myapp.score4)**2-(myapp.score6)**2, (myapp.score5)**2-(myapp.score6)**2, -((255/10)**2)],
                  [(myapp.score7)**2-(myapp.score9)**2, (myapp.score8)**2-(myapp.score9)**2, -((255/10)**2)]];
         const aq=[-[100]*(myapp.score3)**2, -[100]*(myapp.score6)**2, -[100]*(myapp.score9)**2];
         const qs=math.usolve(az, aq);
