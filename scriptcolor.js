5db3c8713c00bba962c6904ed313f9d92b4280c5
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2023-02-16 13:59:27 -0500

    Update scriptcolor.js

--- scriptcolor.js
+++ scriptcolor.js
@@ -172,7 +172,7 @@ function createHTML() {
     var h = document.createElement("DIV");
     h.setAttribute("id", "myProgress");
     h.style.width="100%";
-    h.style.backgroundColor= "hwb(40 0% 0%)";
+    h.style.backgroundColor= "linear-gradient(#e66465, #9198e5)";
     document.body.appendChild(h);
     var g = document.createElement("DIV");
     g.setAttribute("id", "myBar");
