f240320079af642e1b16be069781133d40d533d4
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2024-01-29 18:29:54 -0500

    Update scriptcolor.js var to let

--- scriptcolor.js
+++ scriptcolor.js
@@ -192,12 +192,11 @@ ctx.fillStyle = gradient;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 // Function to convert HSL to RGB
 function hslToRgb(h, s, l) {
-    var r, g, b;
-
+    let r, g, b;
     if (s === 0) {
         r = g = b = l; // achromatic
     } else {
-        var hueToRgb = function hueToRgb(p, q, t) {
+        let hueToRgb = function hueToRgb(p, q, t) {
             if (t < 0) t += 1;
             if (t > 1) t -= 1;
             if (t < 1 / 6) return p + (q - p) * 6 * t;
@@ -205,15 +204,12 @@ function hslToRgb(h, s, l) {
             if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
             return p;
         };
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-
+        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        let p = 2 * l - q;
         r = hueToRgb(p, q, h + 1 / 3);
         g = hueToRgb(p, q, h);
         b = hueToRgb(p, q, h - 1 / 3);
     }
-
     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
  }
 };
